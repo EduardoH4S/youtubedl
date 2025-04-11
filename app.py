04cc1044cd3c88c1707fcbb0e867eec538eda58e
EduardoH4S <eduardohub21@gmail.com>
2025-04-11 16:28:09 -0300

    tmp

--- app.py
+++ app.py
@@ -10,16 +10,17 @@ def index():
         url = request.form['url']
         option = request.form['option']
         yt = YouTube(url)
-        
+
         if option == 'video':
             stream = yt.streams.get_highest_resolution()
         else:
             stream = yt.streams.filter(only_audio=True).first()
-        
-        out_file = stream.download()
-        
+
+        temp_dir = "/tmp"
+        out_file = stream.download(output_path=temp_dir)
+
         return send_file(out_file, as_attachment=True)
-        
+
     return render_template('index.html')
 
 if __name__ == '__main__':
